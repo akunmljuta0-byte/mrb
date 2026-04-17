558a8d8175dd595feb42657b0c9dd7a5c2936718
akunmljuta0-byte <akunmljuta0-byte@users.noreply.github.com>
2026-04-17 03:35:00 +0000

    deploy: 1e46221d5c776c6dc51c6b8c47a79814658ee86e

--- _next/static/6Flm682wcGn9JaxD7ntJD/_buildManifest.js
+++ _next/static/6Flm682wcGn9JaxD7ntJD/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mrb//_next/:path+",
+        "destination": "/mrb/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
